be5cf044b60628ce3f7fe0ad31c509ffe87d800a
Yuvraj8090 <242550011+yuvrajkohlioffice@users.noreply.github.com>
2025-12-03 12:54:19 +0530

    Update index.blade.php

--- resources/views/leads/index.blade.php
+++ resources/views/leads/index.blade.php
@@ -2,7 +2,6 @@
     <div x-data="followupModal()" x-cloak class="min-h-screen bg-gradient-to-br from-gray-100 via-white to-gray-200">
         <div class="ml-64 flex justify-center items-start min-h-screen p-6">
             <div class="w-full max-w-7xl space-y-6">
-
                 <!-- Header -->
                 <div
                     class="flex flex-col md:flex-row md:justify-between md:items-center gap-4 bg-gradient-to-r from-blue-500 to-indigo-600 text-white p-6 rounded-xl shadow-lg">
@@ -41,78 +40,47 @@
 
                     </div>
                 </div>
-
-
                 <!-- Success Message -->
                 @if (session('success'))
                     <div class="p-4 bg-green-500 text-white rounded-lg shadow">
                         {{ session('success') }}
                     </div>
                 @endif
-user List 
+                user List
+                <div class="relative inline-block text-left">
+                    <div>
+                        <button type="button"
+                            class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            id="menu-button" aria-expanded="true" aria-haspopup="true" onclick="toggleDropdown()">
+                            Options
+                            <!-- Heroicon name: solid/chevron-down -->
+                            <svg class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                fill="currentColor" aria-hidden="true">
+                                <path fill-rule="evenodd"
+                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </button>
+                    </div>
 
-<div class="relative inline-block text-left">
-  <div>
-    <button
-      type="button"
-      class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-      id="menu-button"
-      aria-expanded="true"
-      aria-haspopup="true"
-      onclick="toggleDropdown()"
-    >
-      Options
-      <!-- Heroicon name: solid/chevron-down -->
-      <svg
-        class="-mr-1 ml-2 h-5 w-5"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        aria-hidden="true"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-          clip-rule="evenodd"
-        />
-      </svg>
-    </button>
-  </div>
-
-  <div
-    class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none hidden"
-    role="menu"
-    aria-orientation="vertical"
-    aria-labelledby="menu-button"
-    tabindex="-1"
-    id="dropdown-menu"
-  >
-    <div class="py-1" role="none">
-        @foreach ($users as $user )
-      <a
-        href="#"
-        class="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100"
-        role="menuitem"
-        tabindex="-1"
-        id="menu-item-0"
-        value="{{ $user->id }}"
-        >{{ $user->name }}</a
-      >
-      @endforeach
-    </div>
-  </div>
-</div>
-
-<script>
-  function toggleDropdown() {
-    const dropdown = document.getElementById('dropdown-menu');
-    dropdown.classList.toggle('hidden');
-  }
-</script>
-
-
-    
-
+                    <div class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none hidden"
+                        role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1"
+                        id="dropdown-menu">
+                        <div class="py-1" role="none">
+                            @foreach ($users as $user)
+                                <a href="#" class="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100"
+                                    role="menuitem" tabindex="-1" id="menu-item-0"
+                                    value="{{ $user->id }}">{{ $user->name }}</a>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+                <script>
+                    function toggleDropdown() {
+                        const dropdown = document.getElementById('dropdown-menu');
+                        dropdown.classList.toggle('hidden');
+                    }
+                </script>
                 <!-- Leads Table -->
                 <div class="bg-white rounded-xl shadow overflow-x-auto p-4">
                     <x-data-table id="Leads-table" :headers="[
@@ -242,7 +210,6 @@ user List
                         @endforeach
                     </x-data-table>
                 </div>
-
             </div>
         </div>
         <x-followup-modal :packgaes="$packages" />
